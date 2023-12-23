abb1f66dcf50e4b487c0d6dc0b1d8d39d8b8222f
chadireoroonu <chadireoroonu@gmail.com>
2023-12-23 19:33:08 +0900

    ✨ Feat: 기본 app.js 세팅

---
+++ calendar/src/App.js
@@ -0,0 +1,23 @@
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.css';
+
+// import Main from './pages/Main';
+// import Sub from './pages/Sub';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>헤더입니다.</p>
+      </header>
+      <body>
+        <p>바디입니다.</p>
+      </body>
+      <footer>
+        <p>푸터입니다.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
