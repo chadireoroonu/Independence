1ae7933458cef4b58e0ad8130c6d8b6324b501c2
chadireoroonu <chadireoroonu@gmail.com>
2023-12-24 16:17:08 +0900

    ✨ Feat: 기본 router 설정

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,22 +1,37 @@
-// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import './App.css';
-
-// import Main from './pages/Main';
-// import Sub from './pages/Sub';
+import Main from './pages/Main';
+import Sub from './pages/Sub';
 
 function App() {
   return (
-    <div>
-      <header>
-        <p>헤더입니다.</p>
-      </header>
-      <body>
-        <p>바디입니다.</p>
-      </body>
-      <footer>
-        <p>푸터입니다.</p>
-      </footer>
-    </div>
+    <Router>
+      <div>
+        <header>
+          <p>헤더입니다.</p>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">메인</Link>
+              </li>
+              <li>
+                <Link to="/sub">서브</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <body>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/sub" element={<Sub />} />
+          </Routes>
+        </body>
+        <footer>
+          <p>푸터입니다.</p>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ calendar/src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div>
+    	<h2>메인페이지</h2>
+			<p>메인 페이지의 내용입니다.</p>
+    </div>
+  );
+}
+
+export default Main;
