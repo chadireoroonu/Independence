61ab48d1079e9201f96b65d1ab9935b09bbf409e
chadireoroonu <chadireoroonu@gmail.com>
2023-12-24 18:24:35 +0900

    💄 Design: Funding page css

--- calendar/src/pages/Funding.js
+++ calendar/src/pages/Funding.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Funding.css'
 
 function Funding() {
   return (
-    <div>
-      <h2>준비중입니다.</h2>
+    <div id='funding'>
+      <h2>준비중입니다</h2>
+      <p>크라우드 펀딩 이후 펀딩 수익금에 대한 사용내역이 공개될 예정입니다.</p>
     </div>
   );
 }
