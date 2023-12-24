bad829759f5e55bd84c7997c34b47b5ec3ea1432
chadireoroonu <chadireoroonu@gmail.com>
2023-12-24 18:06:18 +0900

    ✨ Feat: 필요 페이지 생성

---
+++ calendar/src/pages/Funding.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Funding() {
+  return (
+    <div>
+      <h2>준비중입니다.</h2>
+    </div>
+  );
+}
+
+export default Funding;
---
+++ calendar/src/pages/Info.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Info() {
+  return (
+    <div>
+      <h2>소개페이지</h2>
+    </div>
+  );
+}
+
+export default Info;
