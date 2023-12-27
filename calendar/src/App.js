7113000bea5b53cf48537a06077c6b0834356221
chadireoroonu <chadireoroonu@gmail.com>
2023-12-27 18:20:21 +0900

    ♻️ Refactor: styled components 방식으로 리팩토링

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import './App.css';
+import styled from 'styled-components';
 import Main from './pages/Main';
 import Info from './pages/Info';
 import Funding from './pages/Funding';
@@ -9,17 +9,17 @@ function App() {
   return (
     <Router>
       <div id='app'>
-        <div id='header'>
-          <h3>
+        <Header>
+          <HeaderTitle>
             <Link to="/">독립달력</Link>
-          </h3>
-          <h3>
+          </HeaderTitle>
+          <HeaderTitle>
             <Link to="/info">소개</Link>
-          </h3>
-          <h3>
+          </HeaderTitle>
+          <HeaderTitle>
             <Link to="/funding">운용</Link>
-          </h3>
-        </div>
+          </HeaderTitle>
+        </Header>
         <div id='body'>
           <Routes>
             <Route path="/" element={<Main />} />
@@ -27,27 +27,74 @@ function App() {
             <Route path="/funding" element={<Funding />} />
           </Routes>
         </div>
-        <div id='footer'>
-          <div id='footer-first-line'>
-            <div class='inner-footer-line'>
+        <Footer>
+          <FooterFirstLine>
+            <InnerFooterLine>
               <p>CSES Fellowship 5기</p>
-            </div>
-            <div class='inner-footer-first-line'>
+            </InnerFooterLine>
+            <InnerFooterFirstLine>
               <p>만년형 우리나라 독립달력</p>
-            </div>
-          </div>
-          <div id='footer-second-line'>
-            <div class='inner-footer-line'>
+            </InnerFooterFirstLine>
+          </FooterFirstLine>
+          <FooterSecondLine>
+            <InnerFooterLine>
               <p>이소정</p>
-            </div>
-            <div class='inner-footer-line'>
-              <p>문의 : chadireoroonu@gmail.com</p>
-            </div>
-          </div>
-        </div>
+            </InnerFooterLine>
+            <InnerFooterLine>
+              <p>문의: chadireoroonu@gmail.com</p>
+            </InnerFooterLine>
+          </FooterSecondLine>
+        </Footer>
       </div>
     </Router>
   );
 }
+
+const Header = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  background-color: #f5f5f5;
+  padding: 10px;
+`;
+
+const HeaderTitle = styled.h3`
+  margin: 0 10px;
+`;
+
+const Footer = styled.div`
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #323232;
+  color: white;
+  font-size: 10pt;
+  padding: 10px;
+`;
+
+const FooterFirstLine = styled.div`
+  width: 100%;
+  justify-content: center;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: -2%;
+`;
+
+const InnerFooterLine = styled.div`
+  display: flex;
+  padding: 0 1%;
+`;
+
+const InnerFooterFirstLine = styled(InnerFooterLine)``;
+
+const FooterSecondLine = styled.div`
+  width: 100%;
+  justify-content: center;
+  display: flex;
+  flex-direction: row;
+`;
 
 export default App;
--- calendar/src/pages/Main.js
+++ calendar/src/pages/Main.js
@@ -1,18 +1,39 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
-import './Main.css';
+import styled from 'styled-components';
 import 'react-calendar/dist/Calendar.css';
 
 function Main() {
 	const [date, setDate] = useState(new Date());
+	
   return (
-    <div id='main'>
-    	<h2>메인페이지</h2>
-			<div>
-				<Calendar setDate={setDate} date={date}/>
-			</div>
-    </div>
+		<MainContainer>
+      <h2>메인페이지</h2>
+      <CalendarBox>
+        <Calendar
+          setDate={setDate}
+          date={date}
+					/>
+      </CalendarBox>
+    </MainContainer>
   );
 }
+
+export const CalendarNavigation = styled.div`
+	background: ${({ theme }) => theme.color.pink};
+	border-bottom: 4px solid ${({ theme }) => theme.color.brown};
+	height: 90px;
+`;
+
+const MainContainer = styled.div`
+	width: 98%;
+	min-height: 90vh;
+	margin: 0 1%;
+`;
+
+const CalendarBox = styled.div`
+	display: flex;
+	justify-content: center;
+`;
 
 export default Main;
