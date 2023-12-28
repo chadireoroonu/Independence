8a60d7a062c79552dca57e44487025ee0d0aa808
chadireoroonu <chadireoroonu@gmail.com>
2023-12-29 04:20:55 +0900

    💄 Design: 달력 여백 등 디자인

--- calendar/src/pages/Main.js
+++ calendar/src/pages/Main.js
@@ -81,6 +81,10 @@ function Main() {
   return (
     <MainContainer>
       <CalendarContainer>
+				<UpperCalendar>
+					<UpperRed/>
+					<UpperBlue/>
+				</UpperCalendar>
         <Calendar
           onChange={setDate}
           value={date}
@@ -89,6 +93,10 @@ function Main() {
 					tileClassName={({ date, view }) => dateClassName(date)}
 					tileContent={({ date, view }) => dateTileContent({ date, view })}
         />
+				<LowerCalendar>
+					<LowerRed/>
+					<LowerBlue/>
+				</LowerCalendar>
       </CalendarContainer>
 			<Modal
         isOpen={modalOpen}
@@ -107,26 +115,110 @@ const MainContainer = styled.div`
   margin: 0 1%;
 `;
 
+const UpperCalendar = styled.div`
+  width: 100%;
+  height: 0.7em;
+	display: flex;
+	flex-direction: row;
+  background-color: black;
+`;
+
+const UpperRed = styled.div`
+	flex: 3;
+	height: 100%;
+	background-color: #E8415B;
+`;
+
+const UpperBlue = styled.div`
+	flex: 2;
+	height: 100%;
+	background-color: #1C4A9F;
+`;
+
+const LowerCalendar = styled.div`
+  width: 100%;
+  height: 0.7em;
+	display: flex;
+	flex-direction: row;
+  background-color: black;
+`;
+
+const LowerRed = styled.div`
+	flex: 2;
+	height: 100%;
+	background-color: #E8415B;
+`;
+
+const LowerBlue = styled.div`
+	flex: 3;
+	height: 100%;
+	background-color: #1C4A9F;
+`;
+
 const CalendarContainer = styled.div`
   display: flex;
+	flex-direction: column;
   justify-content: center;
   margin: 1% auto;
   width: 80%;
 
   .react-calendar {
+		color: #221E1E;
     width: 100%;
     min-height: 58vh;
     border: 1px solid #eeeeee;
   }
 
+	.react-calendar__navigation {
+		min-height: 4.5em;
+		padding: 0 2.5em;
+
+		.react-calendar__navigation__label {
+			font-size: 20px;
+			font-weight: bold;
+		}
+	}
+
+	.react-calendar__month-view__weekdays__weekday {
+		min-height: 3em;
+    font-size: 15px;
+		font-weight: bold;
+  }
+	
   .react-calendar__tile {
     /* each cell css */
     line-height: 7vh;
     padding: 0 0 20px 0;
+		border: 2px solid transparent;
 		position: relative;
   }
+
+	// 선택타일
+	.react-calendar__tile:enabled:hover,
+  .react-calendar__tile:enabled:focus,
+  .react-calendar__tile--active {
+    background: #ffffff;
+		color: #221E1E;
+		border: 2px solid #A59D9D;
+		border-radius: 10px;
+		
+  }
+
+	// 현재타일
+	.react-calendar__tile--now {
+			background: transparent;
+			border: 2px solid #221E1E;
+			border-radius: 10px;
+	}
+
+	// 주말
+	.react-calendar__month-view__weekdays__weekday,
+  .react-calendar__month-view__days__day--weekend { 
+    color: #221E1E;
+  }
+
 	.highlight {
-		background-color: red;
+		// background-color: red;
 	}
 }
 `;
