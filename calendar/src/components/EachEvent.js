2b538a4e4ed74827dc3572588110adc6eb917ae1
chadireoroonu <chadireoroonu@gmail.com>
2023-12-29 07:59:55 +0900

    ✨ Feat: 태그 검색 바로가기 랜덤 이미지 연결

--- calendar/src/components/EachEvent.js
+++ calendar/src/components/EachEvent.js
@@ -2,26 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 
 function EachEvent({ event }) {
+  const randomNum1 = Math.floor(Math.random() * 21) + 1;
+  const randomNum2 = Math.floor(Math.random() * 21) + 1;
+  const randomNum3 = Math.floor(Math.random() * 21) + 1;
+
+  console.log(randomNum1);
+
+  
   console.log(event)
     return (
       event &&
       <EachEventContainer>
         <EventTitle>
-          <h3>{event.year}</h3>
+          <h3>{event.year}년</h3>
           <h5>{event.place}</h5>
         </EventTitle>
         <p>{event.event}</p>
         <SearchLinkContainer>
           <SearchLink href={`https://search.naver.com/search.naver?query=${encodeURIComponent(event.tag)}`} target="_blank" rel="noopener noreferrer">
-            <img src="/path/to/naver-logo.png" alt="Naver logo" />
+            <img src={`/tagImgs/${randomNum1}.png`} />
             <span>네이버에서 자세히보기</span>
           </SearchLink>
           <SearchLink href={`https://www.google.com/search?q=${encodeURIComponent(event.tag)}`} target="_blank" rel="noopener noreferrer">
-            <img src="/path/to/google-logo.png" alt="Google logo" />
+          <img src={`/tagImgs/${randomNum2}.png`} />
             <span>구글에서 자세히보기</span>
           </SearchLink>
           <SearchLink href={`https://search.daum.net/search?q=${encodeURIComponent(event.tag)}`} target="_blank" rel="noopener noreferrer">
-            <img src="/path/to/daum-logo.png" alt="Daum logo" />
+          <img src={`/tagImgs/${randomNum3}.png`} />
             <span>다음에서 자세히보기</span>
           </SearchLink>
         </SearchLinkContainer>
@@ -33,16 +40,17 @@ const EachEventContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+  margin: 0 0 1em 0;
 `;
 
 const EventTitle = styled.div`
-display: flex;
-flex-direction: row;
-margin-bottom: -1em;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: -1em;
 
-h3 {
-  margin-right: 1em;
-}
+  h3 {
+    margin-right: 1em;
+  }
 `;
 
 const SearchLinkContainer = styled.div`
@@ -56,8 +64,7 @@ const SearchLink = styled.a`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: em;
-  height: 10em;
+  width: 20em;
   color: #221E1E;
 	border: 2px solid #A59D9D;
 	border-radius: 10px;
@@ -71,14 +78,15 @@ const SearchLink = styled.a`
   }
 
   img {
-    width: 50px;
-    height: 50px;
-    margin-bottom: 10px;
+    width: 95%;
+    margin: 0.5em 0;
   }
 
   span {
     text-align: center;
+    margin: 0.5em;
   }
+
 `;
   
 export default EachEvent;
