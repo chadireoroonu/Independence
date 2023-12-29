c23833e7739620dbfdf693dff574fb5a8c097ce2
chadireoroonu <chadireoroonu@gmail.com>
2023-12-29 12:31:25 +0900

    ✨ Feat: 검색 기능 기초 구현

--- calendar/src/components/ForSearch.js
+++ calendar/src/components/ForSearch.js
@@ -1,17 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 function ForSearch() {
-    return (
-      <ForSearchContainer>
-        <img src={`${process.env.REACT_APP_PUBLIC_URL}/search.png`} />
-        <SearchContainer>
-          <SearchInput type="text" placeholder="검색 키워드를 입력하세요"/>
-          <SearchButton type="submit">검색</SearchButton>
-        </SearchContainer>
-      </ForSearchContainer>
-    );
-  }
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  console.log(search);
+  console.log(results);
+  console.log(results.year)
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_PUBLIC_URL}/data.json`)
+      .then(response => response.json())
+      .then(json => setData(json));
+  }, []);
+
+  const findAllInObject = (obj, searchText, year, month, day) => {
+    let result = [];
+    
+    const searchInItem = (item) => {
+      if (typeof item === 'string' && item.toLowerCase().includes(searchText.toLowerCase())) {
+        result.push({event: obj, year, month, day});
+      } else if (Array.isArray(item)) {
+        item.forEach(i => searchInItem(i));
+      } else if (typeof item === 'object' && item !== null) {
+        Object.entries(item).forEach(([key, value]) => {
+          if (Number.isInteger(parseInt(key))) {
+            day = key;
+          }
+          searchInItem(value);
+        });
+      }
+    };
+  
+    searchInItem(obj);
+  
+    return result;
+  };
+  
+  // handleSearch 함수를 아래와 같이 수정합니다.
+  const handleSearch = () => {
+    if (search !== '') {
+      let filteredData = [];
+      data.forEach(({month, days}) => {
+        Object.entries(days).forEach(([day, events]) => {
+          events.forEach(event => {
+            const foundItems = findAllInObject(event, search, event.year, month, day);
+            filteredData = [...filteredData, ...foundItems];
+          });
+        });
+      });
+      setResults(filteredData);
+    } else {
+      setResults([]);
+    }
+  };
+
+  return (
+    <ForSearchContainer>
+      <img src={`${process.env.REACT_APP_PUBLIC_URL}/search.png`} />
+      <SearchContainer>
+        <SearchInput
+          type="text"
+          placeholder="검색 키워드를 입력하세요"
+          onChange={e => setSearch(e.target.value)}
+        />
+        <SearchButton type="submit" onClick={handleSearch}>검색</SearchButton>
+      </SearchContainer>
+      {results.map((result, index) => (
+        <div key={index}>
+            <p>{result.year}년 {result.month}월 {result.day}일</p>
+            <p>{result.event.event}</p>
+        </div>
+        ))}
+    </ForSearchContainer>
+  );
+}
   
 const ForSearchContainer = styled.div`
   position: relative;
