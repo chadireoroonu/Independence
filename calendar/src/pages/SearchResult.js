2ea3f69c3c434d659abfdb9524bcbb0df92ea3f9
chadireoroonu <chadireoroonu@gmail.com>
2024-01-02 00:15:26 +0900

    ✨ Feat: 검색 결과 포털 사이트 바로가기 기능 추가

--- calendar/src/pages/SearchResult.js
+++ calendar/src/pages/SearchResult.js
@@ -7,6 +7,8 @@ function SearchResult() {
   const results = location.state?.results || [];
   const keyword = location.state?.keyword || '';
 
+  console.log(results)
+
   return (
     <SearchResultContainer>
       <ForSearch />
@@ -15,8 +17,24 @@ function SearchResult() {
       </CountContainer>
       {results.map((result, index) => (
         <EachResult key={index}>
-          <h4>{result.year}년 {result.month}월 {result.day}일</h4>
-          <p>{result.event.event}</p>
+          <InfoContainer>
+            <h4>{result.year}년 {result.month}월 {result.day}일</h4>
+            <p>{result.event.event}</p>
+          </InfoContainer>
+          <SearchLinkContainer>
+            <SearchLink href={`https://search.naver.com/search.naver?query=${encodeURIComponent(result.event.tag)}`} target="_blank" rel="noopener noreferrer">
+              <img src={`${process.env.PUBLIC_URL}/logoImgs/naver.png`} />
+            </SearchLink>
+            <SearchLink href={`https://www.google.com/search?q=${encodeURIComponent(result.event.tag)}`} target="_blank" rel="noopener noreferrer">
+              <img src={`${process.env.PUBLIC_URL}/logoImgs/google.png`} />
+            </SearchLink>
+            <SearchLink href={`https://search.daum.net/search?q=${encodeURIComponent(result.event.tag)}`} target="_blank" rel="noopener noreferrer">
+              <img src={`${process.env.REACT_APP_PUBLIC_URL}/logoImgs/daum.png`} />
+            </SearchLink>
+            <SearchLink href={`https://www.bing.com/search?q=${encodeURIComponent(result.event.tag)}`} target="_blank" rel="noopener noreferrer">
+              <img src={`${process.env.REACT_APP_PUBLIC_URL}/logoImgs/bing.png`} />
+            </SearchLink>
+          </SearchLinkContainer>
         </EachResult>
       ))}
     </SearchResultContainer>
@@ -38,7 +56,49 @@ const CountContainer = styled.div`
 `;
 
 const EachResult = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const InfoContainer = styled.div`
   margin: 1em 0;
+`;
+
+const SearchLinkContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin: 1em 0
+`;
+
+const SearchLink = styled.a`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 3em;
+  color: #221E1E;
+	border: 2px solid #A59D9D;
+	border-radius: 10px;
+  // box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  text-decoration: none;
+  color: inherit;
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  img {
+    width: 95%;
+    margin: 0.5em 0;
+  }
+
+  span {
+    text-align: center;
+    margin: 0.5em;
+  }
+
 `;
 
 export default SearchResult;
