a1495ef0644e76e844e6239a403129fe4c98b560
chadireoroonu <chadireoroonu@gmail.com>
2024-01-10 02:46:43 +0900

    💄 Design: 분류 선택 CSS

--- calendar/src/pages/Info.js
+++ calendar/src/pages/Info.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 function Info() {
+  const [option, setOption] = useState("all");
+
   return (
-    <InfoContainer id='funding'>
-      <h2>정보 페이지</h2>
+    <InfoContainer>
+      {/* 향후 사진 교체 */}
+      <img src={`${process.env.REACT_APP_PUBLIC_URL}/aboutImgs/title.png`} alt="메인 사진" />
+      <ContentsContainer>
+        <GuideContainer>
+          <h2>분류</h2>
+          <OptionContainer onClick={() => setOption("all")} selected={option === "all"}>
+            <h4>전체</h4>
+          </OptionContainer>
+          <OptionContainer onClick={() => setOption("public")} selected={option === "public"}>
+            <h4>공공</h4>
+          </OptionContainer>
+          <OptionContainer onClick={() => setOption("private")} selected={option === "private"}>
+            <h4>민간</h4>
+          </OptionContainer>
+        </GuideContainer>
+        <CardContainer>
+          <p>card</p>
+        </CardContainer>
+      </ContentsContainer>
     </InfoContainer>
   );
 }
@@ -14,9 +34,54 @@ const InfoContainer = styled.div`
   width: 98%;
   min-height: 100vh;
   margin: 0 1%;
-  justify-content: center;
   align-items: center;
   flex-direction: column;
+  
+  img {
+    width: 100%;
+  }
+`;
+
+const ContentsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  margin: 2em 0;
+`;
+
+const GuideContainer = styled.div`
+  display: flex;
+  box-sizing: border-box;
+  flex-direction: column;
+  width: 20%;
+  padding: 0 1.5em;
+`;
+
+const OptionContainer = styled.div`
+  width: 100%;
+  background-color: ${props => props.selected ? '#F4EEEE' : '#ffffff'};
+
+  h4 {
+    padding: 0 1em;
+    display: inline-block;
+    // transition: transform 0.3s ease-in-out;
+  }
+
+  &:hover {
+    background-color: #F4EEEE;
+    
+    h4 {
+      transform: scale(1.1);
+    }
+  }
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  box-sizing: border-box;
+  flex-direction: column;
+  width: 80%;
+  padding: 0 1em;
 `;
 
 export default Info;
